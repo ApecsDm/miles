5504e420ee46b34ce8df0c83b8692f931f073663
ApecsDm <0133953@bk.ru>
2022-02-02 22:25:42 +0300

    added Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 1500;
+        int amountforonebonus = 20;
+        int bonus = ticketPrice / amountforonebonus;
+        System.out.println(bonus);
+    }
+
 }
