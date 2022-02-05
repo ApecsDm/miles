a9fb5c1967e4f6df1aa7a32473fca364d6f5f366
ApecsDm <0133953@bk.ru>
2022-02-05 18:29:10 +0300

    added autotest BonusMilesService

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 1500;
-        int amountforonebonus = 20;
-        int bonus = ticketPrice / amountforonebonus;
-        System.out.println(bonus);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
-
 }
